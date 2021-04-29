232c126f6f4641a4f306eb441d0b7dfa688dfcd9
idonans <idonans@126.com>
2021-04-29 10:48:28 +0800

    add ContentLoadingFrameLayout example

--- app/src/main/java/io/github/idonans/example/lang/MainActivity.java
+++ app/src/main/java/io/github/idonans/example/lang/MainActivity.java
@@ -1,38 +1,22 @@
 package io.github.idonans.example.lang;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.github.idonans.example.lang.databinding.ActivityMainBinding;
-import io.github.idonans.lang.util.ViewUtil;
-import io.github.idonans.lang.widget.ContentLoadingViewHelper;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ContentLoadingViewHelper mContentLoadingViewHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        mContentLoadingViewHelper = new ContentLoadingViewHelper() {
-            @Override
-            protected void onShow() {
-                ViewUtil.setVisibilityIfChanged(binding.progressBar, View.VISIBLE);
-            }
-
-            @Override
-            protected void onHide() {
-                ViewUtil.setVisibilityIfChanged(binding.progressBar, View.GONE);
-            }
-        };
-        mContentLoadingViewHelper.setMinDelayShowTimeMs(1000);
-        mContentLoadingViewHelper.setMinShowTimeMs(2000);
-        binding.showProgress.setOnClickListener(v -> mContentLoadingViewHelper.show());
-        binding.hideProgress.setOnClickListener(v -> mContentLoadingViewHelper.hide());
+        binding.progressBar.setMinDelayShowTimeMs(1000);
+        binding.progressBar.setMinShowTimeMs(2000);
+        binding.showProgress.setOnClickListener(v -> binding.progressBar.show());
+        binding.hideProgress.setOnClickListener(v -> binding.progressBar.hide());
     }
 
 }
